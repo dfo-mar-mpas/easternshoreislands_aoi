df6886e1e3801c2a2e13af173c37017079d190cf
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2025-02-06 13:59:05 -0400

    remove the buffer from the map

--- code/ESI 2025 CSAS/acoustic_maps.R
+++ code/ESI 2025 CSAS/acoustic_maps.R
@@ -2,17 +2,24 @@
 
 #load libaries
 library(tidyverse)
+library(terra)
+library(tidyterra)
 library(sf)
 library(ggspatial)
 library(rnaturalearth)
 library(MarConsNetData)
 library(scales)
 library(patchwork)
+library(ggnewscale)
+library(knitr)
 
 #Load projections
 latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 utmkm <- "+proj=utm +zone=20 +datum=NAD83 +units=km +no_defs +ellps=GRS80 +towgs84=0,0,0"
 CanProj <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=63.390675 +lon_0=-91.86666666666666 +x_0=6200000 +y_0=3000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+
+#load the ESI dem
+esi_dem <- rast("data/Bathymetry/esi_dem_with_sheetharbour.tiff")
 
 #Basemaps
 bioregion <- data_planning_areas()%>%
@@ -113,9 +120,6 @@ combo_lims[1] <- min(c(estuary_lims[1],release_plot_lims[1]))
 combo_lims[2] <- min(c(estuary_lims[2],release_plot_lims[2]))
 combo_lims[3] <- max(c(estuary_lims[3],release_plot_lims[3]))
 combo_lims[4] <- max(c(estuary_lims[4],release_plot_lims[4]))
-
-
-  
           
 #load and trim rivers db
 river_df <- read_sf("data/Shapefiles/Nova Scotia Topographic DataBase - Water Features (Line Layer)/geo_export_03d8a2fe-2e4e-4497-a8b5-a6ed75aa8b3d.shp")
@@ -136,7 +140,9 @@ west_river <- river_trimmed%>%
 river_coords <- st_coordinates(west_river)
 closed_line <- rbind(river_coords, river_coords[1,])
 west_river_polygon <- st_polygon(list(closed_line[,1:2])) %>%
-                      st_sfc(crs = st_crs(west_river))
+                      st_sfc(crs = st_crs(west_river))%>%
+                      st_make_valid()%>%
+                      st_intersection(combo_lims%>%st_as_sfc())
 
 #now make the islands in the river -- TBH I am not sure how this works but it does
 island_combined <- river_trimmed%>%
@@ -154,12 +160,43 @@ release_locations_combined <- release_locations %>%
 
 #make the plots 
 
+p1_dem <- ggplot()+ #ESI zoom out 
+  geom_sf(data=coastline_esi,fill="grey")+
+  geom_spatraster(data=esi_dem,show.legend = FALSE)+
+  scale_fill_viridis(na.value = "transparent")+
+  ggnewscale::new_scale_fill()+
+  geom_sf(data=release_locations_combined,aes(fill=species),shape=21,size=0)+
+  geom_sf(data= west_river_polygon,fill="white")+
+  geom_sf(data=esi_poly,fill=NA)+
+  geom_sf(data=reciever_locations%>%filter(!grepl("SR",station),!grepl("MR",station)),size=0.2)+
+  geom_sf(data=reciever_locations%>%filter(grepl("SR",station)|grepl("MR",station)),size=1)+
+  geom_sf(data=combo_lims%>%st_as_sfc(),fill=NA,lty=2,linewidth=0.5)+
+  theme_bw()+
+  coord_sf(xlim=reciever_lims[c(1,3)],ylim=reciever_lims[c(2,4)],expand=0)+
+  annotation_scale()+
+  theme(legend.position = "inside",
+        legend.title = element_blank(),
+        legend.position.inside = c(0.17,0.9),
+        legend.background = element_blank())
+
+p1_dem_legend <- ggplot()+
+                 geom_spatraster(data=esi_dem)+
+                 scale_fill_viridis(na.value = "transparent")+
+                 theme_bw()+
+                 theme(legend.position = "bottom",
+                       legend.background = )+
+                 
+
+ggsave("output/ESI_2025_CSAS/dem_legend_only.png", p1_dem_legend, width = 5, height = 6, dpi = 600)
+
+
 p1 <- ggplot()+ #ESI zoom out 
   geom_sf(data=coastline_esi,fill="grey")+
   geom_sf(data=release_locations_combined,aes(fill=species),shape=21,size=0)+
-  geom_sf(data= river_polygon,fill="white")+
+  geom_sf(data= west_river_polygon,fill="white")+
   geom_sf(data=esi_poly,fill=NA)+
-  geom_sf(data=reciever_locations)+
+  geom_sf(data=reciever_locations%>%filter(!grepl("SR",station),!grepl("MR",station)),size=0.2)+
+  geom_sf(data=reciever_locations%>%filter(grepl("SR",station)|grepl("MR",station)),size=1)+
   geom_sf(data=combo_lims%>%st_as_sfc(),fill=NA,lty=2,linewidth=0.5)+
   theme_bw()+
   coord_sf(xlim=reciever_lims[c(1,3)],ylim=reciever_lims[c(2,4)],expand=0)+
@@ -171,7 +208,7 @@ p1 <- ggplot()+ #ESI zoom out
 
 p2 <- ggplot()+ #tag release location zoom in
   geom_sf(data=coastline_esi,fill="grey")+
-  geom_sf(data= river_polygon,fill="white")+
+  geom_sf(data= west_river_polygon,fill="white")+
   geom_sf(data=reciever_locations,size=0.4)+
   geom_sf(data= island_polygon_sf,fill="grey")+
   geom_sf(data=river_trimmed%>%filter(!rivname_1 %in% c("West River Sheet Harbour")),col="grey30",lwd=0.25)+
@@ -181,21 +218,26 @@ p2 <- ggplot()+ #tag release location zoom in
   annotation_scale()+
   labs(fill="")+
   theme(
-    # legend.title = element_blank(),
-    # legend.box = element_blank(),
-    # legend.background = element_rect(fill = scales::alpha("white", 0.5), color = NA),
-    # legend.position = "inside",  # Removes extra padding inside the legend box
-    # legend.position.inside = c(0.18,0.5),
     legend.position = "none",
     axis.title = element_blank(),
     axis.ticks = element_blank(),
     axis.text = element_blank(),
-    plot.margin = margin(0, 0, 0, 0, "pt")
+    plot.margin = margin(0, 0, 0, 0, "pt"),
+    plot.background = element_blank()
   )
 
 #now combine the plots 
 
+#without dem
 combo_plot <- p1 + inset_element(p2, left = 0.405, bottom = -0.015, right = 0.97, top = 0.49)
 
 ggsave("output/ESI_2025_CSAS/acoustic_combo.png",combo_plot,height=5,width=7,units="in",dpi=600)
+
+#with the dem
+combo_plot_dem <- p1_dem + inset_element(p2, left = 0.405, bottom = -0.015, right = 0.97, top = 0.49)
+
+ggsave("output/ESI_2025_CSAS/acoustic_combo_dem.png",combo_plot_dem,height=5,width=7,units="in",dpi=600)
+
+
+
 
